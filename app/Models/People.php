01be1835d295ec7b393891fc1c70e24da73bd1a8
scaduda <samira.caduda@souunit.com.br>
2022-01-31 18:46:25 -0300

    add: use HasEvents na model People

--- app/Models/People.php
+++ app/Models/People.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,6 +11,7 @@ class People extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use HasEvents;
 
     protected $keyType = 'string';
     protected $table = 'people';
